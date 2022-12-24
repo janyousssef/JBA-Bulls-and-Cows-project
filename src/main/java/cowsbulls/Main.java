d0c21e26149b4efebcd4eeb3bff6ba72ffc7b5a5
janyousssef <89477@must.edu.eg.com>
2022-12-24 05:35:35 +0200

    stage 2 completed

--- src/main/java/cowsbulls/Main.java
+++ src/main/java/cowsbulls/Main.java
@@ -1,19 +1,65 @@
 package cowsbulls;
 
+import java.util.Scanner;
+
 class Code {
     private String code;
+    private final int codeLength = 4;
 
-    public String makeCode() {
-        return null;
+    public Code() {
+        makeCode();
     }
 
-    public int bulls() {
-        return 1;
+    public String grade(String input) {
+        String res = "";
+
+        int bulls = calculateBulls(input);
+        int cows = CalculateCows(input) - bulls;
+
+        if (bulls > 0) {
+            res += bulls + " bull(s)";
+        }
+        if (cows > 0) {
+            if (!res.isEmpty()) res += " and ";
+            res += cows + " cows(s)";
+        }
+
+
+        return buildResult(res);
+
     }
 
-    public int cows() {
-        return 1;
+    private String buildResult(String res) {
+        String s = ". The secret code is " + code + ".";
+
+        if (!res.isEmpty())
+            return res + s;
+        return "None" + s;
     }
+
+    private int calculateBulls(String input) {
+        int num = 0;
+        for (int i = 0; i < codeLength; i++) {
+            if (code.charAt(i) == input.charAt(i)) num++;
+        }
+        return num;
+    }
+
+    private int CalculateCows(String input) {
+        int num = 0;
+        for (int i = 0; i < codeLength; i++) {
+            for (int j = 0; j < codeLength; j++) {
+                if (input.charAt(i) == code.charAt(j)) num++;
+            }
+        }
+        return num;
+    }
+
+    private void makeCode() {
+        code = "9305";
+
+    }
+
 }
 
 class Game {
@@ -25,15 +71,20 @@ class Game {
 }
 
 public class Main {
+
+
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
         Game game = new Game();
         Code code = new Code();
-        System.out.println("The secret code is prepared: ****.");
-        for (int i = 0; i < 4; i++) {
-            System.out.printf("Turn %d. Answer:%n", game.getAndIncrementTurn());
-            System.out.println("4734");
-            System.out.println("Grade: " + code.bulls() + " bull and " + code.cows() + " cow.");
-        }
-        System.out.println("Congrats! The secret code is 9876.");
+
+        //System.out.println("The secret code is prepared: ****.");
+
+        System.out.println(code.grade(input));
+
+        //System.out.println("Congrats! The secret code is 9876.");
     }
 }
